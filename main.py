29998fe2a347a175b4382005e209eb2994fa22a4
Zouhour Bellamine <zouhourbellamine13@gmail.com>
2025-12-27 13:01:37 +0100

    data ingestion edit

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 
 from mlflowProject import logger
+from mlflowProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
 
-logger.info("welcome") 
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
